9391b52246577e32cf213850ceeeedd4bee1faee
MachiganMC <machigan@mail.msim.be>
2025-08-08 19:06:58 +0200

    style(file): change exception thrown in generation
    When using the method FileUtils#generateFileFromResourcesIfNotExists if
    the file was not present in the resources, there was only a
    NullPointerException.
    Now there is a FileNotFoundException with the path (and name) of the
    file that was not found in the resources.

--- src/main/java/be/machigan/craftplugin/utils/FileUtils.java
+++ src/main/java/be/machigan/craftplugin/utils/FileUtils.java
@@ -3,21 +3,25 @@ package be.machigan.craftplugin.utils;
 import be.machigan.craftplugin.CraftPlugin;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Objects;
 
 public class FileUtils {
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static void generateFileFromResourcesIfNotExists(File file) throws IOException {
         String dataFolder = CraftPlugin.getPlugin().getDataFolder().getPath();
         String resourcesPath = file.getPath().replace(dataFolder, "");
-        if (!file.exists()) {
+        if (file.exists()) return;
+        try (InputStream resourcesToCopy = CraftPlugin.getPlugin().getClass().getResourceAsStream(resourcesPath)) {
+            if (resourcesToCopy == null)
+                throw new FileNotFoundException("File '" + resourcesPath + "' not found in resources");
             file.mkdirs();
             Files.copy(
-                    Objects.requireNonNull(CraftPlugin.getPlugin().getClass().getResourceAsStream(resourcesPath)),
+                    resourcesToCopy,
                     Paths.get(file.getPath()),
                     StandardCopyOption.REPLACE_EXISTING
             );
